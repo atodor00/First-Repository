326b455782dd55e54b8f51f2ec85bc85bd838d14
atodor00 <atodor00@fesb.hr>
2020-10-08 20:02:09 +0200

    added read and changed main little bit

--- test000/test000/Source.cpp
+++ test000/test000/Source.cpp
@@ -1,6 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+void read(int *);
+
 int main() {
+	int number = 0;
+	int *num = NULL;
+	num = &number;
 	printf("olla sekai");
-	return 0;
-	
+	read(num);
+	return 0;	
+}
+
+
+void read(int* o){
+	scanf(" %d", o);
 }
